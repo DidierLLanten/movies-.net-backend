b92d41c4286407dc8bcede5427bb074dbf8803b3
DidierLLantenPc <didiervelez@hotmail.es>
2023-05-18 21:10:05 -0500

    S5-78 validacion de atributos y de modelo, personalizadas

--- back-end/Entidades/Genero.cs
+++ back-end/Entidades/Genero.cs
@@ -2,12 +2,25 @@
 
 namespace back_end.Entidades
 {
-    public class Genero
+    public class Genero : IValidatableObject
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="El campo {0} es requerido")]
-        [StringLength(maximumLength:8, ErrorMessage ="El campo {0} acepta maximo 10 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 8, ErrorMessage = "El campo {0} acepta maximo 10 caracteres")]
+        // [PrimeraLetraMayuscula]
         public String? Nombre { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("The first letter must be Capital Letter", new String[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
