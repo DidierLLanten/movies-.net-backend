21aa5cc6e1c63694f06b4ba8421ae10189ef20a8
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-18 10:35:59 -0500

    S6-99 configurando recibimiento de actor con imagen

--- back-end/DTOs/ActorCreacionDTO.cs
+++ back-end/DTOs/ActorCreacionDTO.cs
@@ -3,12 +3,12 @@
 namespace back_end.DTOs
 {
     public class ActorCreacionDTO
-    {
-        public int Id { get; set; }
+    {        
         [Required]
         [StringLength(maximumLength: 200)]
         public string Nombre { get; set; }
         public string Biografia { get; set; }
         public DateTime FechaNacimiento { get; set; }
+        public IFormFile Foto { get; set; }
     }
 }
