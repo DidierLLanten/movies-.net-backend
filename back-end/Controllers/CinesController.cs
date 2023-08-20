cc1f715a26ffc9f07ac20b82b95b77089a9e1d16
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-20 17:10:13 -0500

    S6-107 Creando servicio listar con paginacion y eliminar

--- back-end/Controllers/CinesController.cs
+++ back-end/Controllers/CinesController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using back_end.DTOs;
 using back_end.Entidades;
+using back_end.Utilidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace back_end.Controllers
 {
@@ -18,11 +21,32 @@ namespace back_end.Controllers
             this.mapper = mapper;
         }
 
+        [HttpGet] // api/cines
+        public async Task<ActionResult<List<CineDTO>>> Get([FromQuery] PaginacionDTO paginacionDTO)
+        {
+            //logger.LogInformation("Vamos a mostrar los cines, msj LOG----------------------------------------------");            
+            var queryable = context.Cines.AsQueryable();
+            await HttpContext.InsertarParametrosPaginacionEnCabecera(queryable);
+            var cines = await queryable.OrderBy(x => x.Nombre).Paginar(paginacionDTO).ToListAsync();
+            return mapper.Map<List<CineDTO>>(cines);
+        }
+
+
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] CineCreacionDTO cineCreacionDTO)
         {
             var cine = mapper.Map<Cine>(cineCreacionDTO);
             context.Add(cine);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Cines.AnyAsync(x => x.Id == id);
+            if (!existe) { return NotFound(); }
+            context.Remove(new Cine() { Id = id });
             await context.SaveChangesAsync();
             return NoContent();
         }
--- back-end/Utilidades/AutoMapperProfiles.cs
+++ back-end/Utilidades/AutoMapperProfiles.cs
@@ -13,7 +13,9 @@ namespace back_end.Utilidades
             CreateMap<GeneroCreacionDTO, Genero>();
             CreateMap<Actor, ActorDTO>().ReverseMap();
             CreateMap<ActorCreacionDTO, Actor>().ForMember(x => x.Foto, options => options.Ignore());
-            //CreateMap<Cine, CineDTO>().ReverseMap();
+            CreateMap<Cine, CineDTO>()
+                .ForMember(x => x.Latitud, dto => dto.MapFrom(campo => campo.Ubicacion.Y))
+                .ForMember(x => x.Longitud, dto => dto.MapFrom(campo => campo.Ubicacion.X));
             CreateMap<CineCreacionDTO, Cine>().ForMember(x => x.Ubicacion, x => x.MapFrom(dto => geometryFactory.CreatePoint(new Coordinate(dto.Longitud, dto.Latitud))));
         }
     }
