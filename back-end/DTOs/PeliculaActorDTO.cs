c06d5819209a2894a0eb8d7c9c66e86fb3c557e5
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-24 13:49:38 -0500

    S6-113 Creando servicio para mostrar detalle pelicula

--- back-end/DTOs/PeliculaActorDTO.cs
+++ back-end/DTOs/PeliculaActorDTO.cs
@@ -4,8 +4,10 @@ namespace back_end.DTOs
 {
     public class PeliculaActorDTO
     {
-        public int Id { get; set; }        
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Foto { get; set; }
+        public string Personaje { get; set; }
+        public int Orden { get; set; }
     }
 }
